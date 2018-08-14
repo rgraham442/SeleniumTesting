c08de09a3e6e8bbb7497b00127a8ca8f2234b99f
Admin <Admin@MIS>
2018-08-14 16:16:14 +0100

    Added reports to draggable and selectable tests

--- QAToolsSite/src/qatools/interactableTest.java
+++ QAToolsSite/src/qatools/interactableTest.java
@@ -2,8 +2,12 @@ package qatools;
 
 import static org.junit.Assert.*;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -15,46 +19,88 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class interactableTest {
 	
 	private WebDriver driver;
-	  @BeforeClass
-		public static void init() {
-			System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		}
+	private static ExtentReports extent;
+	private static String webAddress;
+	
+	@BeforeClass
+	public static void init() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		
-		@Before
-		public void setUp() {
-			driver = new ChromeDriver();
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		extent = new ExtentReports("C:\\Users\\Admin\\External Apps\\reports\\extentReportDraggable.html", true);
+		ExtentTest initialisationInformation = extent.startTest("Initialisation method");
+		
+		try (FileInputStream file = new FileInputStream("C:\\Users\\Admin\\External Apps\\reports\\files\\reporting.xlsx");
+				XSSFWorkbook myBook = new XSSFWorkbook(file);) {
+			initialisationInformation.log(LogStatus.INFO, "Workbook found");
+			webAddress = myBook.getSheetAt(0).getRow(1).getCell(1).getStringCellValue();
+			initialisationInformation.log(LogStatus.INFO, "Web Address found, value: " + webAddress);
+			initialisationInformation.log(LogStatus.PASS, "DDT Success");
+
+		} catch (FileNotFoundException e) {
+			initialisationInformation.log(LogStatus.FATAL, "Workbook File not found");
+			initialisationInformation.log(LogStatus.INFO, "workbook path: C:\\Users\\Admin\\External Apps\\reports\\files\\reporting.xlsx");
+		} catch (IOException e) {
+			initialisationInformation.log(LogStatus.FATAL, "Could not create workbook");
 		}
+
+	}
+	
+	@Before
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
 	  @Test
 	  public void droppableTest() {
-			driver.get("http://demoqa.com/");
-		    driver.findElement(By.linkText("Droppable")).click();
-		    WebElement drag = driver.findElement(By.cssSelector("#draggableview"));
-		    WebElement drop = driver.findElement(By.cssSelector("#droppableview"));
-		    
-		    Actions builder = new Actions(driver);
+		  ExtentTest extentOGTest = extent.startTest("Droppable default test");
+		  driver.get(webAddress);
+		  extentOGTest.log(LogStatus.INFO, "Navigating to " + webAddress);
+			
+		  driver.findElement(By.linkText("Droppable")).click();
+		  extentOGTest.log(LogStatus.INFO, "Switching to the correct UI");
+		  
+		  WebElement drag = driver.findElement(By.cssSelector("#draggableview"));
+		  WebElement drop = driver.findElement(By.cssSelector("#droppableview"));
+		  
+		  Actions builder = new Actions(driver);
 
-		    Action dragAndDrop = builder.clickAndHold(drag)
-		       .release(drop)
-		       .build();
+		  Action dragAndDrop = builder.clickAndHold(drag)
+		     .release(drop)
+		     .build();
 
-		    dragAndDrop.perform();
+		  dragAndDrop.perform();
+		  extentOGTest.log(LogStatus.INFO, "Dragging and dropping the element in the correct place");
 		    
-		    String result = driver.findElement(By.cssSelector("#droppableview > p")).getText();
-		    assertEquals("The item was not dropped correctly", result, "Dropped!");
+		  String result = driver.findElement(By.cssSelector("#droppableview > p")).getText();
+		  try {
+		  	assertEquals("The item was not dropped correctly", result, "Dropped!");
+			extentOGTest.log(LogStatus.PASS, "Element can successfully drop into other element, navigated successfully");
+		} catch (AssertionError e) {
+			extentOGTest.log(LogStatus.FAIL, "Element not displayed");
+			fail();
+		}
 		    
 	  }
 	  
 	  @Test
 	  public void droppable2Test() {
-			driver.get("http://demoqa.com/");
+		  ExtentTest extentOGTest = extent.startTest("Droppable accept test");
+		  driver.get(webAddress);
+			extentOGTest.log(LogStatus.INFO, "Navigating to " + webAddress);
+			
 		    driver.findElement(By.linkText("Droppable")).click();
 		    driver.findElement(By.id("ui-id-2")).click();
+		    extentOGTest.log(LogStatus.INFO, "Switching to the correct UI");
+		    
 		    WebElement dragInvalid = driver.findElement(By.cssSelector("#draggable-nonvalid"));
 		    WebElement drag = driver.findElement(By.cssSelector("#draggableaccept"));
 		    WebElement drop = driver.findElement(By.cssSelector("#droppableaccept"));
@@ -66,26 +112,45 @@ public class interactableTest {
 			       .build();
 
 			dragFail.perform();
+			extentOGTest.log(LogStatus.INFO, "Dragging and dropping the element that is not valid");
 			
-			String result = driver.findElement(By.cssSelector("#droppableaccept > p")).getText();
-			assertNotEquals("The item was not dropped correctly", result, "Dropped!");
-		    
+			String result = driver.findElement(By.cssSelector("#droppableaccept > p")).getText();		    
+			try {
+				assertNotEquals("The item was not dropped correctly", result, "Dropped!");
+				extentOGTest.log(LogStatus.PASS, "Non-valid element did not drop into the other");
+			} catch (AssertionError e) {
+				extentOGTest.log(LogStatus.FAIL, "Element not displayed");
+				fail();
+			}
+			
 		    Action dragPass = builder.clickAndHold(drag)
 		       .release(drop)
 		       .build();
 
 		    dragPass.perform();
+		    extentOGTest.log(LogStatus.INFO, "Dragging and dropping the element that is valid");
 		    
-		    result = driver.findElement(By.cssSelector("#droppableaccept > p")).getText();
-		    assertEquals("The item was not dropped correctly", result, "Dropped!");
+		    result = driver.findElement(By.cssSelector("#droppableaccept > p")).getText();		    
+		    try {
+		    	assertEquals("The item was not dropped correctly", result, "Dropped!");
+				extentOGTest.log(LogStatus.PASS, "Valid element dropped into the other");
+			} catch (AssertionError e) {
+				extentOGTest.log(LogStatus.FAIL, "Element not displayed");
+				fail();
+			}
 		    
 	  }
 	  
 	  @Test
 	  public void droppable3Test() {
-			driver.get("http://demoqa.com/");
+		  ExtentTest extentOGTest = extent.startTest("Droppable propagation test");
+		  driver.get(webAddress);
+			extentOGTest.log(LogStatus.INFO, "Navigating to " + webAddress);
+			
 		    driver.findElement(By.linkText("Droppable")).click();
 		    driver.findElement(By.id("ui-id-3")).click();
+		    extentOGTest.log(LogStatus.INFO, "Switching to the correct UI");
+		    
 		    WebElement dragProp = driver.findElement(By.cssSelector("#draggableprop"));
 		    WebElement dropGreedy = driver.findElement(By.cssSelector("#droppable2-inner"));
 		    WebElement dropGiving = driver.findElement(By.cssSelector("#droppable-inner"));
@@ -97,11 +162,19 @@ public class interactableTest {
 			       .build();
 
 			dragGreed.perform();
+			extentOGTest.log(LogStatus.INFO, "Dragging and dropping to the inner element that accepts to itself");
 			
 			String result = driver.findElement(By.cssSelector("#droppable2-inner > p")).getText();
 			String resultOut = driver.findElement(By.cssSelector("#droppableprop2 > p")).getText();
-			assertNotEquals("The item was not dropped correctly", resultOut, "Dropped!");
-			assertEquals("The item was not dropped correctly", result, "Dropped!");
+			
+			try {
+				assertNotEquals("The item was not dropped correctly", resultOut, "Dropped!");
+				assertEquals("The item was not dropped correctly", result, "Dropped!");
+				extentOGTest.log(LogStatus.PASS, "Only the inner accepted the element");
+			} catch (AssertionError e) {
+				extentOGTest.log(LogStatus.FAIL, "Element not displayed");
+				fail();
+			}
 		    
 		    
 		    Action dragPass = builder.clickAndHold(dragProp)
@@ -109,19 +182,34 @@ public class interactableTest {
 		       .build();
 
 		    dragPass.perform();
+		    extentOGTest.log(LogStatus.INFO, "Dragging and dropping to the inner element that accepts to itself and the outer element");
 		    
 		    result = driver.findElement(By.cssSelector("#droppable-inner > p")).getText();
 		    resultOut = driver.findElement(By.cssSelector("#droppableprop > p")).getText();
 		    assertEquals("The item was not dropped correctly", result, "Dropped!");
 		    assertEquals("The item was not dropped correctly", resultOut, "Dropped!");
 		    
+		    try {
+		    	assertEquals("The item was not dropped correctly", result, "Dropped!");
+			    assertEquals("The item was not dropped correctly", resultOut, "Dropped!");
+				extentOGTest.log(LogStatus.PASS, "Both inner and outer accepted the element");
+			} catch (AssertionError e) {
+				extentOGTest.log(LogStatus.FAIL, "Element not displayed");
+				fail();
+			}
+		    
 	  }	  
 	  
 	  @Test
 	  public void droppable4Test() throws InterruptedException {
-			driver.get("http://demoqa.com/");
+		  ExtentTest extentOGTest = extent.startTest("Droppable revert test");
+		  driver.get(webAddress);
+			extentOGTest.log(LogStatus.INFO, "Navigating to " + webAddress);
+			
 		    driver.findElement(By.linkText("Droppable")).click();
 		    driver.findElement(By.id("ui-id-4")).click();
+		    extentOGTest.log(LogStatus.INFO, "Switching to the correct UI");
+		    
 		    WebElement dragRevert = driver.findElement(By.cssSelector("#draggablerevert"));
 		    int reverterLocationX = dragRevert.getLocation().getX();
 		    int reverterLocationY = dragRevert.getLocation().getY();
@@ -137,9 +225,17 @@ public class interactableTest {
 			       .build();
 
 		    dragReverter.perform();
+		    extentOGTest.log(LogStatus.INFO, "Dragging and dropping the element that returns to its position");
 		    Thread.sleep(2000);
-			assertEquals("The revert X has changed.", dragRevert.getLocation().getX(), reverterLocationX);
-			assertEquals("The revert Y has changed.", dragRevert.getLocation().getY(), reverterLocationY);
+		    extentOGTest.log(LogStatus.INFO, "Waiting 2 seconds");
+			try {
+				assertEquals("The revert X has changed.", dragRevert.getLocation().getX(), reverterLocationX);
+				assertEquals("The revert Y has changed.", dragRevert.getLocation().getY(), reverterLocationY);
+				extentOGTest.log(LogStatus.PASS, "The element reverted to its original position");
+			} catch (AssertionError e) {
+				extentOGTest.log(LogStatus.FAIL, "Element not displayed");
+				fail();
+			}
 		    
 		    
 		    Action dragPass = builder.clickAndHold(dragDropRevert)
@@ -147,17 +243,31 @@ public class interactableTest {
 		       .build();
 
 		    dragPass.perform();
+		    extentOGTest.log(LogStatus.INFO, "Dragging and dropping the element that does not return to its position");
 		    Thread.sleep(2000);
-			assertNotEquals("The revert X has changed.", dragDropRevert.getLocation().getX(), reverterLocation2X);
-			assertNotEquals("The revert Y has changed.", dragDropRevert.getLocation().getY(), reverterLocation2Y);
+		    extentOGTest.log(LogStatus.INFO, "Waiting 2 seconds");
+			
+			try {
+				assertNotEquals("The revert X has changed.", dragDropRevert.getLocation().getX(), reverterLocation2X);
+				assertNotEquals("The revert Y has changed.", dragDropRevert.getLocation().getY(), reverterLocation2Y);
+				extentOGTest.log(LogStatus.PASS, "The element did not revert to its original position");
+			} catch (AssertionError e) {
+				extentOGTest.log(LogStatus.FAIL, "Element not displayed");
+				fail();
+			}
 		    
 	  }	  
 	  
 	  @Test
 	  public void droppable5Test() {
-			driver.get("http://demoqa.com/");
+		  ExtentTest extentOGTest = extent.startTest("Droppable shopping cart test");
+			driver.get(webAddress);
+			extentOGTest.log(LogStatus.INFO, "Navigating to " + webAddress);
+			
 		    driver.findElement(By.linkText("Droppable")).click();
 		    driver.findElement(By.id("ui-id-5")).click();
+		    extentOGTest.log(LogStatus.INFO, "Switching to the correct UI");
+		    
 		    WebElement dragcartShirt = driver.findElement(By.cssSelector("#ui-id-7 > ul > li:nth-child(1)"));
 		    WebElement dragcartBag = driver.findElement(By.cssSelector("#ui-id-9 > ul > li:nth-child(3)"));
 		    WebElement dragcartGadget = driver.findElement(By.cssSelector("#ui-id-11 > ul > li:nth-child(2)"));
@@ -170,6 +280,7 @@ public class interactableTest {
 			       .build();
 
 		    dragShirt.perform();
+		    extentOGTest.log(LogStatus.INFO, "Dragging shirt item to the cart");
 		    
 		    
 		    Action dragBag = builder.clickAndHold(dragcartBag)
@@ -177,21 +288,32 @@ public class interactableTest {
 		       .build();
 
 		    dragBag.perform();
+		    extentOGTest.log(LogStatus.INFO, "Dragging bag item to the cart");
 		    
 		    Action dragGadget = builder.clickAndHold(dragcartGadget)
 				       .release(dropCart)
 				       .build();
 
 			dragGadget.perform();
+			extentOGTest.log(LogStatus.INFO, "Dragging gadget item to the cart");
 			
 			String cart = driver.findElement(By.cssSelector("#cart > div > ol > li")).getText();
 			assertNotEquals("The shopping cart is still empty", cart, "Add your items here");
+			
+			try {
+				assertNotEquals("The shopping cart is still empty", cart, "Add your items here");
+				extentOGTest.log(LogStatus.PASS, "The element(s) were added to the cart");
+			} catch (AssertionError e) {
+				extentOGTest.log(LogStatus.FAIL, "Element not displayed");
+				fail();
+			}
 		    
 	  }	  
 
 	  @After
 	  public void tearDown() {
 	  driver.close();
+	  extent.flush();
 	  }
 
 }
--- QAToolsSite/src/qatools/selectableTest.java
+++ QAToolsSite/src/qatools/selectableTest.java
@@ -1,9 +1,13 @@
 package qatools;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -12,15 +16,38 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class selectableTest {
 	
 	private WebDriver driver;
+	private static ExtentReports extent;
+	private static String webAddress;
+	
 	@BeforeClass
 	public static void init() {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		
+		extent = new ExtentReports("C:\\Users\\Admin\\External Apps\\reports\\extentReport.html", true);
+		ExtentTest initialisationInformation = extent.startTest("Initialisation method");
+		
+		try (FileInputStream file = new FileInputStream("C:\\Users\\Admin\\External Apps\\reports\\files\\reporting.xlsx");
+				XSSFWorkbook myBook = new XSSFWorkbook(file);) {
+			initialisationInformation.log(LogStatus.INFO, "Workbook found");
+			webAddress = myBook.getSheetAt(0).getRow(1).getCell(1).getStringCellValue();
+			initialisationInformation.log(LogStatus.INFO, "Web Address found, value: " + webAddress);
+			initialisationInformation.log(LogStatus.PASS, "DDT Success");
+
+		} catch (FileNotFoundException e) {
+			initialisationInformation.log(LogStatus.FATAL, "Workbook File not found");
+			initialisationInformation.log(LogStatus.INFO, "workbook path: C:\\Users\\Admin\\External Apps\\reports\\files\\reporting.xlsx");
+		} catch (IOException e) {
+			initialisationInformation.log(LogStatus.FATAL, "Could not create workbook");
+		}
+
 	}
 		
 	@Before
@@ -31,21 +58,76 @@ public class selectableTest {
 	}
 		
 	 @Test
-	  public void selectableTest() {
-			driver.get("http://demoqa.com/");
-		    driver.findElement(By.linkText("Selectable")).click();
-		    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Serialize'])"
-		    		+ "[1]/following::div[2]")).click();
+	  public void defaultTest() {
+		ExtentTest extentOGTest = extent.startTest("Default selector test");
+		driver.get(webAddress);
+		extentOGTest.log(LogStatus.INFO, "Navigating to " + webAddress);
+		driver.findElement(By.linkText("Selectable")).click();
+		driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Serialize'])"
+		   		+ "[1]/following::div[2]")).click();
+		extentOGTest.log(LogStatus.INFO, "Clicking selectable element");
 		    
-		    WebElement item = driver.findElement(By.cssSelector("#selectable > li:nth-child(3)"));
-		    
-		    System.out.println(item.getCssValue("background-color"));
+		WebElement item = driver.findElement(By.cssSelector("#selectable > li.ui-widget-content.ui-corner-left.ui-selectee.ui-selected"));
+		
+		try {
+			assertNotEquals("The item was not selected", item.getCssValue("background-color"), "rgba(255,255,255,1)");
+			extentOGTest.log(LogStatus.PASS, "Element is selectable, navigated successfully");
+		} catch (AssertionError e) {
+			extentOGTest.log(LogStatus.FAIL, "Element not displayed");
+			fail();
+		}
 		    
 	  }
+	 
+	 @Test
+	  public void gridTest() {
+		 ExtentTest extentOGTest = extent.startTest("Grid selector test");
+			driver.get(webAddress);
+			extentOGTest.log(LogStatus.INFO, "Navigating to " + webAddress);
+		    driver.findElement(By.linkText("Selectable")).click();
+		    driver.findElement(By.id("ui-id-2")).click();
+		    extentOGTest.log(LogStatus.INFO, "Switching to the grid selector");
+		    driver.findElement(By.cssSelector("#selectable_grid > li:nth-child(1)")).click();
+		    extentOGTest.log(LogStatus.INFO, "Selecting the element");
+		    
+		    WebElement item = driver.findElement(By.cssSelector("#selectable_grid > li.ui-state-default.ui-corner-left.ui-selectee.ui-selected"));
+		    
+		    try {
+				assertNotEquals("The item was not selected", item.getCssValue("background-color"), "rgba(255,255,255,1)");
+				extentOGTest.log(LogStatus.PASS, "Element is selectable, navigated successfully");
+			} catch (AssertionError e) {
+				extentOGTest.log(LogStatus.FAIL, "Element not displayed");
+				fail();
+			}
+	 }
+	 
+	 @Test
+	  public void serialTest() {
+		 ExtentTest extentOGTest = extent.startTest("Seial selector test");
+			driver.get(webAddress);
+			extentOGTest.log(LogStatus.INFO, "Navigating to " + webAddress);
+		    driver.findElement(By.linkText("Selectable")).click();
+		    driver.findElement(By.id("ui-id-3")).click();
+		    extentOGTest.log(LogStatus.INFO, "Switching to the serialize selector");
+		    driver.findElement(By.cssSelector("#selectable-serialize > li:nth-child(1)")).click();
+		    extentOGTest.log(LogStatus.INFO, "Selecting the element");
+		    
+		    String result = driver.findElement(By.cssSelector("#select-result")).getText();
+		    
+		    assertNotEquals("The item was not selected", result, "none");
+		    try {
+		    	assertNotEquals("The item was not selected", result, "none");
+				extentOGTest.log(LogStatus.PASS, "Element is selectable, result changes based on element, navigated successfully");
+			} catch (AssertionError e) {
+				extentOGTest.log(LogStatus.FAIL, "Element not displayed");
+				fail();
+			}
+	 }
 
 	@After
 	public void tearDown() {
 	  driver.close();
+	  extent.flush();
 	}
 
 }
